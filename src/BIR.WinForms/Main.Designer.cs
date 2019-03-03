c60c2ae93f263335f37b3398b3c827efccf14dad
BlaiseRienzo <brienzo@live.com>
2019-03-03 15:09:16 -0500

    Validation prior to resize. Layout cleaned up slightly. Move resize action to the Process button.

--- src/BIR.WinForms/Main.Designer.cs
+++ src/BIR.WinForms/Main.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.btnProcess = new System.Windows.Forms.Button();
-            this.sourceFolderPicker = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderPicker = new System.Windows.Forms.FolderBrowserDialog();
             this.btnSelectSource = new System.Windows.Forms.Button();
             this.lbSourceFiles = new System.Windows.Forms.ListBox();
             this.lbBatchFiles = new System.Windows.Forms.ListBox();
@@ -39,20 +39,29 @@
             this.lblHeight = new System.Windows.Forms.Label();
             this.txtWidth = new System.Windows.Forms.TextBox();
             this.lblWidth = new System.Windows.Forms.Label();
+            this.rbtnResizeModeStretch = new System.Windows.Forms.RadioButton();
+            this.gbResizeMode = new System.Windows.Forms.GroupBox();
+            this.rbtnResizeModeCover = new System.Windows.Forms.RadioButton();
+            this.rbtnResizeModeContain = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblBatchFiles = new System.Windows.Forms.Label();
+            this.gbResizeMode.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnProcess
             // 
-            this.btnProcess.Location = new System.Drawing.Point(135, 405);
+            this.btnProcess.Location = new System.Drawing.Point(344, 236);
             this.btnProcess.Name = "btnProcess";
-            this.btnProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess.Size = new System.Drawing.Size(173, 23);
             this.btnProcess.TabIndex = 0;
             this.btnProcess.Text = "Process";
             this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
             // 
             // btnSelectSource
             // 
-            this.btnSelectSource.Location = new System.Drawing.Point(12, 26);
+            this.btnSelectSource.Location = new System.Drawing.Point(12, 12);
             this.btnSelectSource.Name = "btnSelectSource";
             this.btnSelectSource.Size = new System.Drawing.Size(140, 23);
             this.btnSelectSource.TabIndex = 2;
@@ -64,24 +73,24 @@
             // 
             this.lbSourceFiles.DisplayMember = "Name";
             this.lbSourceFiles.FormattingEnabled = true;
-            this.lbSourceFiles.Location = new System.Drawing.Point(12, 61);
+            this.lbSourceFiles.Location = new System.Drawing.Point(12, 47);
             this.lbSourceFiles.Name = "lbSourceFiles";
             this.lbSourceFiles.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
-            this.lbSourceFiles.Size = new System.Drawing.Size(140, 212);
+            this.lbSourceFiles.Size = new System.Drawing.Size(140, 186);
             this.lbSourceFiles.TabIndex = 1;
             // 
             // lbBatchFiles
             // 
-            this.lbBatchFiles.DisplayMember = "FullName";
+            this.lbBatchFiles.DisplayMember = "Name";
             this.lbBatchFiles.FormattingEnabled = true;
-            this.lbBatchFiles.Location = new System.Drawing.Point(170, 61);
+            this.lbBatchFiles.Location = new System.Drawing.Point(170, 47);
             this.lbBatchFiles.Name = "lbBatchFiles";
             this.lbBatchFiles.Size = new System.Drawing.Size(160, 212);
             this.lbBatchFiles.TabIndex = 3;
             // 
             // btnAddToBatch
             // 
-            this.btnAddToBatch.Location = new System.Drawing.Point(12, 290);
+            this.btnAddToBatch.Location = new System.Drawing.Point(12, 236);
             this.btnAddToBatch.Name = "btnAddToBatch";
             this.btnAddToBatch.Size = new System.Drawing.Size(140, 23);
             this.btnAddToBatch.TabIndex = 4;
@@ -91,17 +100,17 @@
             // 
             // btnSelectOutputFolder
             // 
-            this.btnSelectOutputFolder.Location = new System.Drawing.Point(386, 26);
+            this.btnSelectOutputFolder.Location = new System.Drawing.Point(344, 12);
             this.btnSelectOutputFolder.Name = "btnSelectOutputFolder";
             this.btnSelectOutputFolder.Size = new System.Drawing.Size(173, 23);
             this.btnSelectOutputFolder.TabIndex = 5;
-            this.btnSelectOutputFolder.Text = "Select Output";
+            this.btnSelectOutputFolder.Text = "Select Destination";
             this.btnSelectOutputFolder.UseVisualStyleBackColor = true;
             this.btnSelectOutputFolder.Click += new System.EventHandler(this.btnSelectOutputFolder_Click);
             // 
             // txtHeight
             // 
-            this.txtHeight.Location = new System.Drawing.Point(230, 337);
+            this.txtHeight.Location = new System.Drawing.Point(58, 54);
             this.txtHeight.Name = "txtHeight";
             this.txtHeight.Size = new System.Drawing.Size(100, 20);
             this.txtHeight.TabIndex = 6;
@@ -109,7 +118,7 @@
             // lblHeight
             // 
             this.lblHeight.AutoSize = true;
-            this.lblHeight.Location = new System.Drawing.Point(183, 340);
+            this.lblHeight.Location = new System.Drawing.Point(11, 57);
             this.lblHeight.Name = "lblHeight";
             this.lblHeight.Size = new System.Drawing.Size(41, 13);
             this.lblHeight.TabIndex = 7;
@@ -117,7 +126,7 @@
             // 
             // txtWidth
             // 
-            this.txtWidth.Location = new System.Drawing.Point(230, 364);
+            this.txtWidth.Location = new System.Drawing.Point(58, 19);
             this.txtWidth.Name = "txtWidth";
             this.txtWidth.Size = new System.Drawing.Size(100, 20);
             this.txtWidth.TabIndex = 8;
@@ -125,29 +134,104 @@
             // lblWidth
             // 
             this.lblWidth.AutoSize = true;
-            this.lblWidth.Location = new System.Drawing.Point(183, 367);
+            this.lblWidth.Location = new System.Drawing.Point(11, 22);
             this.lblWidth.Name = "lblWidth";
             this.lblWidth.Size = new System.Drawing.Size(38, 13);
             this.lblWidth.TabIndex = 9;
             this.lblWidth.Text = "Width:";
             // 
+            // rbtnResizeModeStretch
+            // 
+            this.rbtnResizeModeStretch.AutoSize = true;
+            this.rbtnResizeModeStretch.Location = new System.Drawing.Point(6, 65);
+            this.rbtnResizeModeStretch.Name = "rbtnResizeModeStretch";
+            this.rbtnResizeModeStretch.Size = new System.Drawing.Size(59, 17);
+            this.rbtnResizeModeStretch.TabIndex = 10;
+            this.rbtnResizeModeStretch.TabStop = true;
+            this.rbtnResizeModeStretch.Tag = "3";
+            this.rbtnResizeModeStretch.Text = "Stretch";
+            this.rbtnResizeModeStretch.UseVisualStyleBackColor = true;
+            // 
+            // gbResizeMode
+            // 
+            this.gbResizeMode.Controls.Add(this.rbtnResizeModeCover);
+            this.gbResizeMode.Controls.Add(this.rbtnResizeModeContain);
+            this.gbResizeMode.Controls.Add(this.rbtnResizeModeStretch);
+            this.gbResizeMode.Location = new System.Drawing.Point(344, 140);
+            this.gbResizeMode.Name = "gbResizeMode";
+            this.gbResizeMode.Size = new System.Drawing.Size(173, 90);
+            this.gbResizeMode.TabIndex = 11;
+            this.gbResizeMode.TabStop = false;
+            this.gbResizeMode.Text = "Resize Mode";
+            // 
+            // rbtnResizeModeCover
+            // 
+            this.rbtnResizeModeCover.AutoSize = true;
+            this.rbtnResizeModeCover.Location = new System.Drawing.Point(6, 42);
+            this.rbtnResizeModeCover.Name = "rbtnResizeModeCover";
+            this.rbtnResizeModeCover.Size = new System.Drawing.Size(53, 17);
+            this.rbtnResizeModeCover.TabIndex = 12;
+            this.rbtnResizeModeCover.TabStop = true;
+            this.rbtnResizeModeCover.Tag = "1";
+            this.rbtnResizeModeCover.Text = "Cover";
+            this.rbtnResizeModeCover.UseVisualStyleBackColor = true;
+            // 
+            // rbtnResizeModeContain
+            // 
+            this.rbtnResizeModeContain.AutoSize = true;
+            this.rbtnResizeModeContain.Location = new System.Drawing.Point(6, 19);
+            this.rbtnResizeModeContain.Name = "rbtnResizeModeContain";
+            this.rbtnResizeModeContain.Size = new System.Drawing.Size(61, 17);
+            this.rbtnResizeModeContain.TabIndex = 11;
+            this.rbtnResizeModeContain.TabStop = true;
+            this.rbtnResizeModeContain.Tag = "2";
+            this.rbtnResizeModeContain.Text = "Contain";
+            this.rbtnResizeModeContain.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtWidth);
+            this.groupBox1.Controls.Add(this.lblWidth);
+            this.groupBox1.Controls.Add(this.lblHeight);
+            this.groupBox1.Controls.Add(this.txtHeight);
+            this.groupBox1.Location = new System.Drawing.Point(344, 47);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(173, 87);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dimensions";
+            // 
+            // lblBatchFiles
+            // 
+            this.lblBatchFiles.AutoSize = true;
+            this.lblBatchFiles.Location = new System.Drawing.Point(170, 21);
+            this.lblBatchFiles.Name = "lblBatchFiles";
+            this.lblBatchFiles.Size = new System.Drawing.Size(59, 13);
+            this.lblBatchFiles.TabIndex = 13;
+            this.lblBatchFiles.Text = "Batch Files";
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(712, 566);
-            this.Controls.Add(this.lblWidth);
-            this.Controls.Add(this.txtWidth);
-            this.Controls.Add(this.lblHeight);
-            this.Controls.Add(this.txtHeight);
+            this.ClientSize = new System.Drawing.Size(522, 265);
+            this.Controls.Add(this.lblBatchFiles);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gbResizeMode);
             this.Controls.Add(this.btnSelectOutputFolder);
             this.Controls.Add(this.btnAddToBatch);
             this.Controls.Add(this.lbBatchFiles);
             this.Controls.Add(this.btnSelectSource);
             this.Controls.Add(this.lbSourceFiles);
             this.Controls.Add(this.btnProcess);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Main";
             this.Text = "Main";
+            this.gbResizeMode.ResumeLayout(false);
+            this.gbResizeMode.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -157,7 +241,7 @@
         #endregion
 
         private System.Windows.Forms.Button btnProcess;
-        private System.Windows.Forms.FolderBrowserDialog sourceFolderPicker;
+        private System.Windows.Forms.FolderBrowserDialog folderPicker;
         private System.Windows.Forms.Button btnSelectSource;
         private System.Windows.Forms.ListBox lbSourceFiles;
         private System.Windows.Forms.ListBox lbBatchFiles;
@@ -167,5 +251,11 @@
         private System.Windows.Forms.Label lblHeight;
         private System.Windows.Forms.TextBox txtWidth;
         private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.RadioButton rbtnResizeModeStretch;
+        private System.Windows.Forms.GroupBox gbResizeMode;
+        private System.Windows.Forms.RadioButton rbtnResizeModeContain;
+        private System.Windows.Forms.RadioButton rbtnResizeModeCover;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblBatchFiles;
     }
 }
--- src/BIR.WinForms/Main.cs
+++ src/BIR.WinForms/Main.cs
@@ -14,7 +14,8 @@ namespace BIR.WinForms
     public partial class Main : Form
     {
 
-        
+        string srcPath;
+        string destPath;
 
         public Main()
         {
@@ -23,65 +24,95 @@ namespace BIR.WinForms
 
         private void btnSelectSource_Click(object sender, EventArgs e)
         {
-            var result = sourceFolderPicker.ShowDialog();
+            if (!string.IsNullOrWhiteSpace(srcPath)) { folderPicker.SelectedPath = srcPath; }
+
+            var result = folderPicker.ShowDialog();
             if (result != DialogResult.Cancel)
             {
-
+                srcPath = folderPicker.SelectedPath;
                 lbSourceFiles.Items.Clear();
                 lbSourceFiles.DisplayMember = "Name";
-                DirectoryInfo srcFolder = new DirectoryInfo(sourceFolderPicker.SelectedPath);
+                DirectoryInfo srcFolder = new DirectoryInfo(srcPath);
 
-                foreach(FileInfo fi in srcFolder.GetFiles())
+                foreach (FileInfo fi in srcFolder.GetFiles().Where(fi=> (new string[]{ ".jpg",".png",".bmp",".tif",".jpeg"}).Contains(fi.Extension.ToLower())))
                 {
-
                     lbSourceFiles.Items.Add(new BIR.Common.Models.ImageReference { Name = fi.Name, FullName = fi.FullName });
                 }
-                               
+
             }
         }
 
         private void btnAddToBatch_Click(object sender, EventArgs e)
         {
-            
-            foreach(BIR.Common.Models.ImageReference ir in lbSourceFiles.SelectedItems)
+            foreach (BIR.Common.Models.ImageReference ir in lbSourceFiles.SelectedItems)
             {
-
-                    lbBatchFiles.Items.Add(ir);
-
+                lbBatchFiles.Items.Add(ir);
             }
-
-
         }
 
         private void btnSelectOutputFolder_Click(object sender, EventArgs e)
         {
-            var result = sourceFolderPicker.ShowDialog();
-            if (result != DialogResult.Cancel)
+            if (!string.IsNullOrWhiteSpace(destPath))
             {
-
-                foreach (BIR.Common.Models.ImageReference ir in lbBatchFiles.Items)
-                {
-
-                    Image srcImage = Image.FromFile(ir.FullName);
-
-                    var resized = BIR.Common.ImageUtility.ResizeImage(srcImage, Convert.ToInt32(txtWidth.Text), Convert.ToInt32(txtHeight.Text), Common.Enums.ResizeMode.Contain);
-
-
-                    resized.Save(Path.Combine(sourceFolderPicker.SelectedPath, ir.Name),System.Drawing.Imaging.ImageFormat.Jpeg);
-                    
-
-
-
-                }
-
-
+                folderPicker.SelectedPath = destPath;
+            }
+            else if (!string.IsNullOrWhiteSpace(srcPath))
+            {
+                folderPicker.SelectedPath = srcPath;
             }
 
-
-
+            var result = folderPicker.ShowDialog();
+            if (result != DialogResult.Cancel)
+            {
+                destPath = folderPicker.SelectedPath;
+            }
 
         }
 
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
 
+            int targetWidth;
+            int targetHeight;
+            bool resizeModeSet = false;
+            Common.Enums.ResizeMode resizeMode = Common.Enums.ResizeMode.Contain;
+
+            if (!Int32.TryParse(txtHeight.Text, out targetHeight) || !(Int32.TryParse(txtWidth.Text, out targetWidth)))
+            {
+                MessageBox.Show("Height and Width must be set", "Error", MessageBoxButtons.OK);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(destPath))
+            {
+                MessageBox.Show("Select a destination path", "Error", MessageBoxButtons.OK);
+                return;
+            }
+
+
+            foreach (RadioButton rbtn in gbResizeMode.Controls)
+            {
+                if (rbtn.Checked)
+                {
+                    resizeMode = (Common.Enums.ResizeMode)Convert.ToInt32((rbtn.Tag));
+                    resizeModeSet = true;
+                }
+            }
+            if (!resizeModeSet)
+            {
+                MessageBox.Show("Resize Mode must be selected", "Error", MessageBoxButtons.OK);
+                return;
+            }
+
+
+            foreach (BIR.Common.Models.ImageReference ir in lbBatchFiles.Items)
+            {
+                Image srcImage = Image.FromFile(ir.FullName);
+                var resized = BIR.Common.ImageUtility.ResizeImage(srcImage, targetWidth, targetHeight, resizeMode);
+                resized.Save(Path.Combine(destPath, ir.Name), System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+
+            MessageBox.Show("Batch omplete", "Notice", MessageBoxButtons.OK);
+        }
     }
 }
